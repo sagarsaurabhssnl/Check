420038fff0a44f6e7c0f8960e185c2692fd1cca3
sagarsaurabhssnl <56288082+sagarsaurabhssnl@users.noreply.github.com>
2021-01-10 09:39:22 +0530

    Add files via upload

---
+++ js/playstate.js
@@ -0,0 +1,19 @@
+//FUNCTION TO BE EXECUTED AT GAMESTATE PLAY
+function gamestateplay() {
+    if (gamestate === play) {
+        gameover.visible = false;
+        fire();
+        playermovement();
+        zombiespawn();
+        zombiedown();
+        peopleSpawn();
+        coinSpawn();
+        gameend();
+        push();
+        fill(150);
+        rect(camera.position.x - 100, camera.position.y + 300, 200, 10);
+        pop();
+        fill("red");
+        rect(camera.position.x - 100, camera.position.y + 300, health, 10);
+    }
+}
